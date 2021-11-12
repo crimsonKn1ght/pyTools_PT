939bf92b0fbec217bd95713dc0af9e28f6f5e24e
Gourab <87464911+confusedHatHacker@users.noreply.github.com>
2021-11-13 00:32:20 +0530

    Update portScan.py
    some changes

--- portScan.py
+++ portScan.py
@@ -1,48 +1,62 @@
-#!/usr/bin/env python3
+#!/usr/share/env python3
 
-import socket,sys,pyfiglet,time,concurrent.futures
+#This code will be modified shortly so that it can also 
+#grab banners for ports, even tho a small change of sock.recv(1024)
+#would be enough, I'm not sure how good itwork with threading
 
-# ascii_banner = pyfiglet.figlet_format("Tryhackme \nPython 4 pentesters \nScan Start")
-# print(ascii_banner)
+import socket,threading,sys
+from queue import Queue
 
+class portScanner:
+	def __init__(self, target):
+		self.target = target
+		self.open_ports=[]
 
-
-def probe_port(ip, port, result=1):
-	try:
+	def portscan(self, port):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.settimeout(0.5)
-		r = sock.connect_ex((ip,port))
-		if r==0:
-			result=r
-		sock.close()
-	except Exception as e:
-		pass
-	return result
+		try:
+			sock.connect((self.target, port))
+			return 1
+		except:
+			return 0
 
+	def portArrange(self, sport, eport):
+		self.q = Queue()
+		for port in range(int(sport),int(eport)+1):
+			self.q.put(port)
+
+	def worker(self):
+		while not self.q.empty():
+			port = self.q.get()
+			if self.portscan==1:
+				print(f'[+] Port {port} is open')
+				self.open_ports.append(port)
+			else:
+				pass
+
+	def run_scan(self, threads):
+		self.portArrange(sys.argv[2], sys.argv[3])
+		thread_list=[]
+
+		for _ in range(int(threads)):
+			thread = threading.Thread(target=self.worker)
+			thread_list.append(thread)
+
+		for thread in thread_list:
+			thread.start()
+	
+		for thread in thread_list:
+			thread.join()
 
 if __name__=='__main__':
-
-	if len(sys.argv)<2:
-		print("Usage: {sys.argv[0]} <ip address/domain> <port_start> <end_port>")
+	
+	if len(sys.argv)<3:
+		print("Usage: {sys.argv[0]} <ip-address> <start-port> <end-port> <threads(optional)>")
 		exit()
 
-	ip = sys.argv[1]
-	open_ports = []
-	threads = []
+	t=20
+	if sys.argv[4]:
+		t=sys.argv[3]
 
-	ports= range(int(sys.argv[2]), int(sys.argv[3]))
-
-	time1=time.time()
-
-	for port in ports:
-	response=probe_port(ip,port)
-	if response==0:
-		open_ports.append(port)
-
-	if open_ports:
-		print("Open ports: ")
-		print(sorted(open_ports))
-	else:
-		print("All ports closed.")
-
-	print(time.time()-time1)
+	scan = portScanner(sys.argv[1])
+	scan.run_scan(t)
