9dc3138365bd5aa2ddc6e5fdeda259d8b0bc7644
Gourab <87464911+confusedHatHacker@users.noreply.github.com>
2021-11-14 19:08:48 +0530

    Now codes supports threading for faster scan timings
    Added threading at 300, can be modified according to needs

--- portScan.py
+++ portScan.py
@@ -1,62 +1,79 @@
 #!/usr/share/env python3
 
-#This code will be modified shortly so that it can also 
-#grab banners for ports, even tho a small change of sock.recv(1024)
-#would be enough, I'm not sure how good itwork with threading
-
-import socket,threading,sys
+import socket
+import threading
+import sys
 from queue import Queue
 
-class portScanner:
-	def __init__(self, target):
-		self.target = target
-		self.open_ports=[]
+class portDiscovery:
 
-	def portscan(self, port):
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		try:
-			sock.connect((self.target, port))
-			return 1
-		except:
-			return 0
-
-	def portArrange(self, sport, eport):
+	def __init__(self):
 		self.q = Queue()
-		for port in range(int(sport),int(eport)+1):
+		# self.open_ports=[]
+
+	def portScan(self,ip, port):
+		try:
+			scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			scan.settimeout(1.5)
+			scan.connect((ip, port))
+			return True
+		except:
+			return False
+
+	def get_ports(self,ports):
+		for port in range(int(ports)):
 			self.q.put(port)
 
 	def worker(self):
 		while not self.q.empty():
 			port = self.q.get()
-			if self.portscan==1:
-				print(f'[+] Port {port} is open')
-				self.open_ports.append(port)
+			if self.portScan(sys.argv[1], port):
+				open_ports.append(port)
+				print('[+] Open port: {}'.format(port))
 			else:
 				pass
 
-	def run_scan(self, threads):
-		self.portArrange(sys.argv[2], sys.argv[3])
-		thread_list=[]
-
+	def run_scan(self,threads):
+		self.get_ports(sys.argv[2])
+		thread_list = []
+		
 		for _ in range(int(threads)):
 			thread = threading.Thread(target=self.worker)
+			thread.start()
 			thread_list.append(thread)
 
 		for thread in thread_list:
-			thread.start()
-	
-		for thread in thread_list:
 			thread.join()
 
+class bannerGrabber:
+
+	def __init__(self):
+		pass
+
+	def bannerDisplay(self):
+		for port in open_ports:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.settimeout(10)
+			try:
+				sock.connect((sys.argv[1], port))
+				banner = sock.recv(1024)
+				print("Port {}: banner".format(port))
+			except:
+				print("Port {}: Couldn\'t retrieve banner".format(port))
+
+
+
 if __name__=='__main__':
-	
-	if len(sys.argv)<3:
-		print("Usage: {sys.argv[0]} <ip-address> <start-port> <end-port> <threads(optional)>")
-		exit()
 
-	t=20
-	if sys.argv[4]:
-		t=sys.argv[3]
+	if len(sys.argv)<2:
+		print("Usage: python portScanner <ip-address> <	ports>")
+		sys.exit(0)
 
-	scan = portScanner(sys.argv[1])
-	scan.run_scan(t)
+	open_ports = []
+
+	print('--------Starting syn scan--------')
+	portDisc = portDiscovery()
+	portDisc.run_scan(300)
+
+	banners = bannerGrabber()
+	banners.bannerDisplay()
